c7783fb64bb25485a8f6a7e781308bbf1fb81f64
Ryan Liyanage <83898244+RyLiy@users.noreply.github.com>
2024-02-23 02:04:15 -0500

    Update postgres_connect.py

--- postgres_connect.py
+++ postgres_connect.py
@@ -1,33 +1,34 @@
 import psycopg2
 from pip._vendor.distlib import database
 
-#connect to the db
+# Connect to the DB. Fill in with own credentials.
 con = psycopg2.connect(
             host = "192.168.1.101",
             database = 'demo',
             user = 'postgres',
             password = 'password')
 
-#cursor
+# Connection cursor for SQL statements
 cur = con.cursor()
 
-#A sample query commented out.
+# A sample query commented out. Pls ignore
 #cur.execute("insert into orders (date,type) values (%s,%s)",('2022-01-08','sell'))
 
-#execute query
+# Execute query
 cur.execute("select ID, date, type from orders")
 
-#Fetches results from select query
+# Fetches results from select query
 rows = cur.fetchall()
 
-#Will iterate through all rows, and only prints the first 3 columns
+# Will iterate through all rows in result, and only prints the first 3 columns
 for r in rows:
     print(f"ID {r[0]} Date {r[1]} Type {r[2]}")
 
-#commit the transaction
+# commit the transaction. ACID principles!
 con.commit()
 
-#close the cursor
+# close the cursor
 cur.close()
-#close the connection
+# close the connection
 con.close()
+
