03e93550b3c83793ee51d3762659ebf696b23030
Ryan Liyanage <83898244+RyLiy@users.noreply.github.com>
2024-02-23 02:01:34 -0500

    Didn't change code, but added better comments

--- instagramFollowerExtractor.py
+++ instagramFollowerExtractor.py
@@ -1,58 +1,71 @@
-import instaloader, random, time
+import instaloader
+import random
+import time
 from time import sleep
 from threading import Thread
 from os.path import exists
 
+# Global variables
 terminationPoint = False
 running = True
 amount = 0
 totalusers = 1
 
+# Function to iterate over followers and write them to a file
 def iterate():
-    global running 
-    #L = instaloader.Instaloader(rate_controller=lambda ctx: MyRateController(ctx))
+    global running
+    
+    # Initialize Instaloader
     L = instaloader.Instaloader()
     
-    #youraccname = your instagram profile name
-    L.load_session_from_file('youraccname')
-
-    #Username of person we are going to extract all followers from
-    user = ''
+    # Load session from file
+    L.load_session_from_file('youraccname')  # Replace 'youraccname' with your Instagram profile name
     
-    profile = instaloader.Profile.from_username(L.context, user)
+    # Username of the person to extract followers from
+    user = 'username'  # Replace 'username' with the person's IG profile
+    
+    # Open file to write followers' usernames
     f = open('C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Data\\instagram_users\\' + user + '.txt', 'a')
-
-    # Grab an iterator for list of followers
+    
+    # Get profile object
+    profile = instaloader.Profile.from_username(L.context, user)
+    
+    # Get iterator for followers
     user_iterator = profile.get_followers()
-        
-        
+    
+    # Thaw iterator from file if exists
     if exists(user + "_iterator.json"):
         print("Existing iterator was found, loading previous state\n")
         user_iterator.thaw(instaloader.load_structure_from_file(L,user + "_iterator.json"))
     
-    global totalusers
+    global totalusers # Assume global variables
     global amount
     amount = user_iterator.total_index
     totalusers = profile.followers
-    start = time.time()
-    PERIOD_OF_TIME = 12600 #3.5 hours
     
+    # Start time
+    start = time.time()
+    PERIOD_OF_TIME = 12600 # Need to pause every 3.5 hours to avoid rate limiting, currently unused in favor of random sleeping
+    
+    # Iterate over followers
     for followers in user_iterator:
-        # if time.time() > start + PERIOD_OF_TIME:
-            # bad_shutdown(3,user_iterator,user)
-            # break
         amount += 1
         f.write(followers.username + '\n')
         
-        #if amount % 60 == 0:
+        # Save iterator to file every 60 followers
         instaloader.save_structure_to_file(user_iterator.freeze(), user + "_iterator.json")
+        
+        # Random sleep to avoid rate limiting
         sleep(random.uniform(0, 3))
-
-    f.close()
-    if (amount / totalusers) >= 0.99:
-        print("Completed - all usernames retrieved. margin of error - 1%")
     
+    # Close file
+    f.close()
+    
+    # Check if all usernames retrieved
+    if (amount / totalusers) >= 0.99:
+        print("Completed - all usernames retrieved. Margin of error - 1%")
 
+# Function to display progress
 def progress():
     prev = 0
     print ("Initializing: Logging in, retrieving profile, retrieving iterator...\n")
@@ -62,17 +75,16 @@ def progress():
             prev = ((amount/totalusers)*100)
             print (str((amount/totalusers)*100) +'%')
 
+# Create threads
 t1 = Thread(target=iterate)
 t2 = Thread(target=progress)
-# start the threads
+
+# Start the threads
 t1.start()
 t2.start()
 
-
-    
-
-
+# Wait for threads to finish
 t1.join()
 t2.join()
-print ("threads successfully closed")
 
+print ("Threads successfully closed")
